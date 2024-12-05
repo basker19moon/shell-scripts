8356b102c5d23dcccb647f28c4cb6269e9a7c124
Bhaskar Darla <basker19moon@gmail.com>
2024-12-05 11:42:58 +0530

    we could see nice output with this script

--- affects.sh
+++ affects.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+#####################################
+# Author: Bhaskar Darla
+#
+# Date : 05/12/2024
+#
+# Script to display affects on output
+#
+# Version: v1.01
+#####################################
+
+arr=('-' '\' '|' '/')
+while true; do
+	for c in "${arr[@]}"; do 
+		echo -en "\r $c " 
+		sleep .5
+	done
+done
 
