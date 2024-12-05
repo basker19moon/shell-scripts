33f1be044753a392195708909165490ae8d4d645
Bhaskar Darla <basker19moon@gmail.com>
2024-12-05 11:22:02 +0530

    Shell script for addition two number

---
+++ addition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#####################################
+# Author: Bhaskar Darla
+# Date : 05/12/2024
+#
+# Script Addition of Two numbers
+# Version: v1.01
+#####################################
+
+echo -n "Enter the first Number: "
+read -r a 
+echo -n "Enter the second Number: "
+read -r b
+echo "$a + $b = $((a+b))"
